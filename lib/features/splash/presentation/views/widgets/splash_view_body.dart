7474ec28e48d80c7dcf00ead0ac67d6e05274f24
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-11-04 13:55:56 +0200

    Add OnBoardingView and integrate with route generation
    This commit introduces the `OnBoardingView` and its associated widget, `OnBoardingViewBody`, to the project. The `OnBoardingView` is now integrated into the route generation logic within the `onGenerateRoute` function. Additionally, the `SplashViewBody` has been modified to navigate to the `OnBoardingView` after a delay of 3 seconds. This enhancement allows users to smoothly transition from the splash screen to the onboarding process.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:svg_flutter/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,5 +38,11 @@ class SplashViewBody extends StatelessWidget {
         ),
       ]),
     );
+  }
+
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed(OnBoardingView.routeName);
+    });
   }
 }
