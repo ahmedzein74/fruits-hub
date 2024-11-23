edeb59233899e93c233ee4c9fec4cb06c0981c27
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-11-23 20:18:45 +0200

    read and write data from user  finish

--- lib/core/services/database_services.dart
+++ lib/core/services/database_services.dart
@@ -1,4 +1,12 @@
 abstract class DatabaseServices {
   Future<void> addData(
-      {required String path, required Map<String, dynamic> data});
+      {String? documentId,
+      required String path,
+      required Map<String, dynamic> data});
+
+  Future<Map<String, dynamic>> getData(
+      {required String path, required String documentId});
+
+  Future<bool> checkisUserExist(
+      {required String path, required String documentId});
 }
